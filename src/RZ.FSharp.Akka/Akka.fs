4e2595a1abf509382b9f211f074c8d7836d9ef9c
Ruxo Zheng <rz@just.sent.as>
2022-06-04 09:06:18 +0700

    Tested version

--- src/RZ.FSharp.Akka/Akka.fs
+++ src/RZ.FSharp.Akka/Akka.fs
@@ -1,4 +1,38 @@
 ﻿module RZ.FSharp.Akka
 
-type FsActor() =
-    inherit UntypedActor()
+open System
+open System.Threading.Tasks
+open Akka.Actor
+
+type ActorContext = {
+    Self :IActorRef
+    Sender :IActorRef
+    Become :(FsActorReceivable -> unit) -> unit
+}
+and FsActorReceivable =
+    abstract member FsReceive<'T> :(struct (ActorContext * 'T) -> unit) -> unit
+    abstract member FsReceiveAsync<'T> :(struct (ActorContext * 'T) -> Async<unit>) -> unit
+
+type FsFuncReceiver<'T> = (struct (ActorContext * 'T)) -> unit
+type FsFuncAsyncReceiver<'T> = (struct (ActorContext * 'T)) -> Async<unit>
+
+type FsReceiveActor() =
+    inherit ReceiveActor()
+    
+    member my.FsReceive<'T>(f :FsFuncReceiver<'T>) =
+        // TODO: check if Sender is really correct!
+        base.Receive<'T>(fun v -> f (my.GetContext(), v))
+            
+    member my.FsReceiveAsync<'T>(f :FsFuncAsyncReceiver<'T>) =
+        base.ReceiveAsync(fun v -> f (my.GetContext(), v) |> Async.StartImmediateAsTask :> Task)
+        
+    interface FsActorReceivable with
+        member my.FsReceive<'T>(f :FsFuncReceiver<'T>) = my.FsReceive<'T>(f)
+        member my.FsReceiveAsync<'T>(f :FsFuncAsyncReceiver<'T>) = my.FsReceiveAsync<'T>(f)
+            
+    member my.Become (action :Action) = base.Become action
+        
+    member private my.GetContext() =
+        { Self = my.Self
+          Sender = my.Sender
+          Become = fun f -> my.Become (fun _ -> f my)}
